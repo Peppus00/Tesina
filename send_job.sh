a3054574a0cc634e7684192d2f7ff60ef1f84465
Bernardo D'Agostino <b.dagostino2@studenti.unipi.it>
2023-11-15 14:40:14 -0500

    slur script for cluster

---
+++ send_job.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=mipfda
+#SBATCH --cpus-per-task=20
+#SBATCH --time=0-23:00
+#SBATCH --mem=100GB
+#SBATCH --output=Z_000.out.log
+#SBATCH --error=Z_000.err.log
+
+echo "Current working directory: `pwd`"
+echo "Starting run at: `date`"
+
+module load python/3.8.5
+
+srun python Tesina_cluster.py
+
+echo "Job finished with exit code $? at: `date`"
